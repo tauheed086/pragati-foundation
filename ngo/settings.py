98a7f1309dd60e43d61a932ad57aeba28f86a663
Abdul Raheem <tauheedbldeacet@gmail.com>
2024-09-25 17:28:57 +0530

    email deleted

--- ngo/settings.py
+++ ngo/settings.py
@@ -132,9 +132,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '009mulla@gmail.com'  # Your email
-EMAIL_HOST_PASSWORD = 'urng yxlj iaev njom'  # Your email password or app password
